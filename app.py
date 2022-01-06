b3cb2663180f66cc9ebe17b3cd2a894dc5ad6352
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2022-01-06 19:33:35 +0100

    Prepare frontend and connect to backend

--- app.py
+++ app.py
@@ -8,3 +8,19 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("hello.html")
+
+@app.route("/addclient")
+def add_client():
+    return render_template("addclient.html")
+
+@app.route("/addgate")
+def add_gate():
+    return render_template("addgate.html")
+
+@app.route("/clients")
+def clients():
+    return render_template("clients.html")
+
+@app.route("/ongym")
+def on_gym():
+    return render_template("ongym.html")
