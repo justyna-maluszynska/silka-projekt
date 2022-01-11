aa64d3c70fd2e9d4b9f1debb07d5f72fd9db9d60
AgnieszkaKlobus12 <aga.hagneia@gmail.com>
2022-01-11 15:20:09 +0100

    terminals list, add and remove supported

--- app.py
+++ app.py
@@ -1,35 +1,35 @@
-from flask import Flask, render_template
-import time
-from flask_mqtt import Mqtt
-from config import BROKER_URL
+from flask import Flask, render_template, request
 
-from utils import enter_client, get_clients, increase_new_card_number, process_card, register_client, get_clients_on_gym
+# from flask_mqtt import Mqtt
+from config import BROKER_URL
+from utils import get_clients, get_clients_on_gym, get_terminals, add_terminal, remove_terminal
 
 app = Flask(__name__)
 app.config['MQTT_BROKER_URL'] = BROKER_URL
 
-client = Mqtt(app)
 
-
-#zacznij subskrybować temat od razu po połączeniu z brokerem
-@client.on_connect()
-def handle_connect(client, userdata, flags, rc):
-    client.subscribe('card/id')
-
-
-#metoda dla konkretnego tematu
-@client.on_topic('card/id')
-def handle_card_id(client, userdata, message):
-    # Decode message.
-    message_decoded = (str(message.payload.decode("utf-8"))).split(".")
-
-    # Print message to console.
-    if message_decoded[0] != "Client connected" and message_decoded[
-            0] != "Client disconnected":
-        print(time.ctime() + ", " + message_decoded[0] +
-              " used the RFID card.")
-    else:
-        print(message_decoded[0] + " : " + message_decoded[1])
+# client = Mqtt(app)
+#
+#
+# #zacznij subskrybować temat od razu po połączeniu z brokerem
+# @client.on_connect()
+# def handle_connect(client, userdata, flags, rc):
+#     client.subscribe('card/id')
+#
+#
+# #metoda dla konkretnego tematu
+# @client.on_topic('card/id')
+# def handle_card_id(client, userdata, message):
+#     # Decode message.
+#     message_decoded = (str(message.payload.decode("utf-8"))).split(".")
+#
+#     # Print message to console.
+#     if message_decoded[0] != "Client connected" and message_decoded[
+#             0] != "Client disconnected":
+#         print(time.ctime() + ", " + message_decoded[0] +
+#               " used the RFID card.")
+#     else:
+#         print(message_decoded[0] + " : " + message_decoded[1])
 
 
 @app.route("/")
@@ -47,10 +47,30 @@ def add_gate():
     return render_template("addgate.html")
 
 
+@app.route("/addgate_handle", methods=['POST'])
+def addgate_handle():
+    new_id = request.form['terminal_id']
+    gate_type = request.form['type']
+    add_terminal(new_id, gate_type)
+    return terminals()
+
+
+@app.route('/<terminal_id>/remove_gate')
+def remove_gate(terminal_id):
+    remove_terminal(terminal_id)
+    return terminals()
+
+
 @app.route("/clients")
 def clients():
     clients = get_clients()
     return render_template("clients.html", clients=clients)
+
+
+@app.route("/terminals")
+def terminals():
+    terminals = get_terminals()
+    return render_template("terminals.html", terminals=terminals)
 
 
 @app.route("/ongym")
--- utils.py
+++ utils.py
@@ -1,45 +1,45 @@
 import json
-from flask import Flask, render_template, redirect, url_for
 
-from config import DATA_FILENAME
+from config import CLIENTS_DATA_FILENAME, TERMINALS_DATA_FILENAME
 
 
-def load_data():
-    with open(DATA_FILENAME, 'r+') as file:
+def load_data(filename):
+    with open(filename, 'r+') as file:
         file_data = json.load(file)
 
     return file_data
 
 
 def get_clients():
-    file_data = load_data()
+    file_data = load_data(CLIENTS_DATA_FILENAME)
 
     return file_data["clients"]
 
+
 def get_clients_on_gym():
-    file_data = load_data()
+    file_data = load_data(CLIENTS_DATA_FILENAME)
 
     return file_data["active_clients"]
 
 
 def get_new_card_number():
-    file_data = load_data()
+    file_data = load_data(CLIENTS_DATA_FILENAME)
 
     return file_data["next_card_number"]
 
 
 def increase_new_card_number():
-    file_data = load_data()
+    file_data = load_data(CLIENTS_DATA_FILENAME)
     file_data["next_card_number"] += 1
-    with open(DATA_FILENAME, 'w') as file:
+    with open(CLIENTS_DATA_FILENAME, 'w') as file:
         json.dump(file_data, file, indent=4)
 
 
-#dodaj nowego klienta
+# dodaj nowego klienta
 def register_client(new_client):
     clients = get_clients()
     clients.append(new_client)
-    with open(DATA_FILENAME, 'w') as file:
+    with open(CLIENTS_DATA_FILENAME, 'w') as file:
         json.dump(clients, file, indent=4)
 
 
@@ -50,9 +50,9 @@ def get_client_data(card_number: int):
     return client_data
 
 
-#klient wchodzi na siłkę
+# klient wchodzi na siłkę
 def enter_client(card_number, entry_time, client_data):
-    file_data = load_data()
+    file_data = load_data(CLIENTS_DATA_FILENAME)
     file_data['active_clients'].append({
         "name": client_data["name"],
         "surname": client_data["surname"],
@@ -60,26 +60,50 @@ def enter_client(card_number, entry_time, client_data):
         "entry_time": entry_time
     })
 
-    with open(DATA_FILENAME, 'w') as file:
+    with open(CLIENTS_DATA_FILENAME, 'w') as file:
         json.dump(file_data, file, indent=4)
 
 
-#klient wychodzi z siłki
+# klient wychodzi z siłki
 def get_away_client(card_number):
-    file_data = load_data()
+    file_data = load_data(CLIENTS_DATA_FILENAME)
     active_clients = list(filter(lambda x: x["card_number"] != card_number, file_data['active_clients']))
     file_data['active_clients'] = active_clients
 
-    with open(DATA_FILENAME, 'w') as file:
+    with open(CLIENTS_DATA_FILENAME, 'w') as file:
         json.dump(file_data, file, indent=4)
 
 
-#przetwarzanie karty 
+# przetwarzanie karty
 def process_card(card_number, time):
-    file_data = load_data()
+    file_data = load_data(CLIENTS_DATA_FILENAME)
     client = list(filter(lambda x: x["card_number"] == card_number, file_data['active_clients']))
-    
+
     if len(client) > 0:
         get_away_client(card_number)
     else:
         enter_client(card_number, time, client[0])
+
+
+def get_terminals():
+    file_data = load_data(TERMINALS_DATA_FILENAME)
+    return file_data["terminals"]
+
+
+def add_terminal(new_id, gate_type):
+    file_data = load_data(TERMINALS_DATA_FILENAME)
+    file_data['terminals'].append({
+        "terminal_id": new_id,
+        "type": gate_type
+    })
+    with open(TERMINALS_DATA_FILENAME, 'w') as file:
+        json.dump(file_data, file, indent=4)
+
+
+def remove_terminal(terminal_id):
+    file_data = load_data(TERMINALS_DATA_FILENAME)
+    terminals = list(filter(lambda x: x["terminal_id"] != terminal_id, file_data['terminals']))
+    file_data['terminals'] = terminals
+
+    with open(TERMINALS_DATA_FILENAME, 'w') as file:
+        json.dump(file_data, file, indent=4)
