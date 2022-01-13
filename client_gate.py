cf8869748ab042e5605281c1ca0a749ac87eb12d
LeviSforza <3monika100@gmail.com>
2022-01-13 20:17:59 +0100

    Add improvements form meeting

--- client_gate.py
+++ client_gate.py
@@ -4,12 +4,13 @@ from tkinter import CENTER
 import paho.mqtt.client as mqtt
 
 # The terminal ID - can be any string.
-terminal_id = "Gate1"
+terminal_id = "Gate3"
 # The broker name or IP address.
 broker = "localhost"
 # broker = "127.0.0.1"
 # broker = "10.0.0.1"
 
+<<<<<<< Updated upstream
 
 class Record:
     active_list = []
@@ -17,6 +18,10 @@ class Record:
 
 
 current_record = Record()
+=======
+active_list = []
+random_card = 0  # Access through class
+>>>>>>> Stashed changes
 
 # The MQTT client.
 client = mqtt.Client()
@@ -26,16 +31,24 @@ window = tkinter.Tk()
 
 
 def send_message(mess):
-    client.publish("card/id", terminal_id + "." + mess, )
+    client.publish("card/check/" + terminal_id, terminal_id + "." + mess, 2)
 
 
 def send_id():
     send_message("GET_ACTIVE")
     client.on_message = process_message
+<<<<<<< Updated upstream
     if current_record.random_card in current_record.active_list:
         client.publish("card/id", str(current_record.random_card) + "." + "DEACTIVATE", )
     else:
         client.publish("card/id", str(current_record.random_card) + "." + "ACTIVATE", )
+=======
+    time.sleep(1)
+    if random_card in active_list:
+        send_message("DEACTIVATE" + "." + str(random_card))
+    else:
+        send_message("ACTIVATE" + "." + str(random_card))
+>>>>>>> Stashed changes
 
 
 def process_message(client, userdata, message):
